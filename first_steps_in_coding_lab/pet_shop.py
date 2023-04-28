fb0fc87943c9a8bef960214ff1e380483924dc08
vladimir-zlatev <125951708+vladimir-zlatev@users.noreply.github.com>
2023-04-28 12:44:40 -0700

    Added Pet Shop
    Added Pet Shop

---
+++ first_steps_in_coding_lab/pet_shop.py
@@ -0,0 +1,8 @@
+packs_for_dogs = int(input())
+packs_for_cat = int(input())
+
+total_price_cats = packs_for_cat * 4
+total_price_dogs = packs_for_dogs * 2.5
+total_price = total_price_cats + total_price_dogs
+
+print(f"{total_price} lv.")
